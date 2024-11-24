712ab2d6e9d4d293e9537442f8fe24854175b319
Moises Chirinos <mfrchg@hotmail.com>
2024-11-24 17:09:55 -0500

    testing ruta

--- tiendasegura/settings.py
+++ tiendasegura/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print ("############ RUTA ESTATICA ########################")
+print (STATIC_ROOT)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
