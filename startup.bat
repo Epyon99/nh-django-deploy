368ebf0932196f7add8f2b63645a21333f69f751
Moises Chirinos <mfrchg@hotmail.com>
2024-11-24 15:53:36 -0500

    startup.bat con collectstatic

---
+++ startup.bat
@@ -0,0 +1,8 @@
+@echo off
+
+REM Ejecutar collectstatic para recopilar archivos estáticos
+echo Running collectstatic...
+python manage.py collectstatic --noinput
+
+REM Iniciar el servidor Django con Gunicorn
+gunicorn --bind=0.0.0.0:8000 myproject.wsgi:application
